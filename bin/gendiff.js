e644e00a29dcda7f04fef8bf6828e669ed78b1ba
Sergey Troshkin <sergey.troshkin@gmail.com>
2023-04-14 11:41:20 +0300

    Add search for differences between two flat json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 const program = new Command();
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const genDiff = (filepath1, filepath2) => {
+  const obj1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)));
+  const obj2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)));
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  const sortedKeys = _.sortBy(keys);
+
+  const result = sortedKeys.map((key) => {
+    if (!_.has(obj2, key)) {
+      return ` - ${key}: ${obj1[key]}`;
+    } else if (!_.has(obj1, key)) {
+      return ` + ${key}: ${obj2[key]}`;
+    } else if (obj1[key] !== obj2[key]) {
+      return ` - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+    } else {
+      return `   ${key}: ${obj1[key]}`;
+    }
+  });
+
+  return `{\n ${result.join('\n ')}\n}`;
+};
+
+export default genDiff;
