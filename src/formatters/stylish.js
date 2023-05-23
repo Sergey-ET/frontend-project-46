518d2952dafed44a7f73b625d41ebc944af64dfd
Sergey Troshkin <sergey.troshkin@gmail.com>
2023-05-23 16:35:37 +0300

    Refactor stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,19 +1,24 @@
 import _ from 'lodash';
 
 const stylish = (data, depth = 0, indent = 4) => {
+  const calculateIndentation = (key) => {
+    const additionalIndent = (key.startsWith('- ') || key.startsWith('+ ')) ? 2 : 4;
+    const indentationDepth = (depth * indent) + additionalIndent;
+    return ' '.repeat(indentationDepth);
+  };
+
+  const formatValue = (value) => {
+    if (_.isObject(value)) {
+      return `: {\n${stylish(value, depth + 1, indent)}\n${' '.repeat((depth + 1) * indent)}}`;
+    }
+    return value !== '' ? `: ${value}` : ':';
+  };
+
   if (_.isObject(data)) {
     const entries = Object.entries(data);
     const result = entries.map(([key, value]) => {
-      const additionalIndent = (key.startsWith('- ') || key.startsWith('+ ')) ? 2 : 4;
-      const indentationDepth = (depth * indent) + additionalIndent;
-      const indentation = ' '.repeat(indentationDepth);
-
-      let formattedValue;
-      if (_.isObject(value)) {
-        formattedValue = `: {\n${stylish(value, depth + 1)}\n${' '.repeat((depth + 1) * indent)}}`;
-      } else {
-        formattedValue = value !== '' ? `: ${value}` : ':';
-      }
+      const indentation = calculateIndentation(key);
+      const formattedValue = formatValue(value, key);
 
       return `${indentation}${key}${formattedValue}`;
     });
