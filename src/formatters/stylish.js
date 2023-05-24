c39b987f65e33096af29dbdd7dfa08002f73f0b3
Sergey Troshkin <sergey.troshkin@gmail.com>
2023-05-24 16:25:07 +0300

    Refactor stylish function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -11,33 +11,25 @@ const printValue = (value, depth) => {
   return `{\n${lines.join('\n')}\n${indent.slice(0, -2)}}`;
 };
 
+const generateLine = (indent, symbol, key, value, depth) => `${indent.slice(0, -2)}${symbol} ${key}:${value === '' ? '' : ' '}${printValue(value, depth + 1)}`;
+
 const stylish = (diffs, depth = 1) => {
   const indent = ' '.repeat(depth * 4 - 2);
   const currentIndent = `${indent}  `;
-
   const lines = diffs.map((diff) => {
-    let line;
-
     switch (diff.type) {
       case 'deleted':
-        line = `${currentIndent.slice(0, -2)}- ${diff.key}:${diff.value === '' ? '' : ' '}${printValue(diff.value, depth + 1)}`;
-        break;
       case 'added':
-        line = `${currentIndent.slice(0, -2)}+ ${diff.key}:${diff.value === '' ? '' : ' '}${printValue(diff.value, depth + 1)}`;
-        break;
+        return generateLine(currentIndent, diff.type === 'deleted' ? '-' : '+', diff.key, diff.value, depth);
       case 'updated':
-        line = `${currentIndent.slice(0, -2)}- ${diff.key}:${diff.valueBefore === '' ? '' : ' '}${printValue(diff.valueBefore, depth + 1)}\n${currentIndent.slice(0, -2)}+ ${diff.key}:${diff.valueAfter === '' ? '' : ' '}${printValue(diff.valueAfter, depth + 1)}`;
-        break;
+        return `${generateLine(currentIndent, '-', diff.key, diff.valueBefore, depth)}\n${generateLine(currentIndent, '+', diff.key, diff.valueAfter, depth)}`;
       case 'nested':
-        line = `${currentIndent}${diff.key}: ${stylish(diff.children, depth + 1)}`;
-        break;
+        return `${currentIndent}${diff.key}: ${stylish(diff.children, depth + 1)}`;
       case 'unchanged':
-        line = `${currentIndent}${diff.key}:${diff.value === '' ? '' : ' '}${printValue(diff.value, depth + 1)}`;
-        break;
+        return `${currentIndent}${diff.key}:${diff.value === '' ? '' : ' '}${printValue(diff.value, depth + 1)}`;
       default:
-        line = '';
+        return '';
     }
-    return line;
   });
   return `{\n${lines.join('\n')}\n${indent.slice(0, -2)}}`;
 };
